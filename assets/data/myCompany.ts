a02cb52e2e6cac6fe821a282f878aae7c1ecc044
mahmoudramz <52760874+mahmoudramz@users.noreply.github.com>
2019-08-13 17:08:08 -0400

    Add files via upload

---
+++ assets/data/myCompany.ts
@@ -0,0 +1,8 @@
+import {Company} from '../../app/company';
+
+export const COMPANYDATA : Company [] = [
+    {name : 'W&TR',city :'TORONTO', country : 'Canada'},
+    {name : 'MO PAINTING',city :'Brampton', country : 'Canada'},
+    {name : 'Karam Clinic',city :'PICKERING', country : 'Canada'},
+
+]
